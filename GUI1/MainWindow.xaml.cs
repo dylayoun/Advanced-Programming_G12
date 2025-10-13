1537f79d32ff332c3383ed074e913a13c1560876
dylayoun <61238923+dylayoun@users.noreply.github.com>
2025-10-13 12:24:08 +0100

    WPF solution commit

---
+++ GUI1/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GUI1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            HelpWindow hwin = new HelpWindow();
+            hwin.Show();
+        }
+
+        private void SendText_Button_Click(object sender, RoutedEventArgs e)
+        {
+            string input_txt = Input_Text.Text;
+
+// Add the f# stub for the interpreter
+        }
+    }
+}
